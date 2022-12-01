01a580cded3954683dab7d59e8db3713c0d980ae
Anton <anton.wiblishauser@gmx.de>
2022-12-01 17:34:01 +0100

    create DayManager

---
+++ src/main/kotlin/dev/nyon/aoc/Day.kt
@@ -0,0 +1,32 @@
+package dev.nyon.aoc
+
+import kotlin.io.path.Path
+import kotlin.io.path.readText
+
+const val currentDay = 1
+
+@Suppress("SENSELESS_COMPARISON")
+fun main() {
+    val part = 1
+
+    val dayText = Path("src/main/kotlin/dev/nyon/aoc/inputs", "Day$currentDay.txt").readText()
+    getDayObject(dayText, dayText.lines()).also {
+        println(
+            if (part == 1) it.firstPart()
+            else it.secondPart()
+        )
+    }
+}
+
+interface Day{
+    val inputString: String
+    val inputLines: List<String>
+
+    fun firstPart(): Any
+    fun secondPart(): Any
+}
+
+fun getDayObject(inputString: String, inputLines: List<String>): Day = when (currentDay) {
+    1 -> Day01(inputString, inputLines)
+    else -> Day01(inputString, inputLines)
+}
