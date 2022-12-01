59483fdb9377a3dc91d861b6c24e566dd2245031
Anton <anton.wiblishauser@gmx.de>
2022-12-01 17:34:23 +0100

    add day1

---
+++ src/main/kotlin/dev/nyon/aoc/Day01.kt
@@ -0,0 +1,8 @@
+package dev.nyon.aoc
+
+class Day01(override val inputString: String, override val inputLines: List<String>) : Day {
+    private val summed = inputString.split("\n\n").map { s -> s.lines().sumOf { it.toInt() } }
+    override fun firstPart(): Int = summed.max()
+
+    override fun secondPart(): Int = summed.sorted().takeLast(3).sum()
+}
